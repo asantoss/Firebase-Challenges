c3a46c91ce034371bd73f09799856352b4d9e44f
Alexander Santos <alexsantosantana@live.com>
2020-03-17 18:32:09 -0400

    initia commit teaching how to interface witht the firestore database

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
+var app = firebase.initializeApp(firebaseConfig);
+var db = firebase.firestore();
 
+var commentForm = document.getElementById('commentForm');
 
+commentForm.addEventListener('submit', function(event) {
+	event.preventDefault();
+	console.log(event);
+	var comment = event.target.elements.comment.value;
+	return db
+		.collection('comments')
+		.add({
+			body: comment
+		})
+		.then(function(doc) {
+			// *** We receive the refrence to the document as the parameter of our callback that runs after the promise is resolved.
+			var commentData = doc.data();
+			var commentHTML = `
+            <div class="comment">
+                ${commentData.body}
+            </div>
 
-
+        `;
+		});
+});
